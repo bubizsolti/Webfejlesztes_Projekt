b9de9a55d9b9d39345ab2731f2bd76a9ea20b4ce
Bubenkó Zsolt <bubizsolti@mailbox.unideb.hu>
2024-12-21 11:27:58 +0100

    Index.html megcsinálása

--- src/main/java/parfumApp/Controller/ParfumController.java
+++ src/main/java/parfumApp/Controller/ParfumController.java
@@ -1,24 +1,47 @@
 package parfumApp.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import parfumApp.Repository.ParfumRepository;
 import parfumApp.model.Parfum;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@RestController
+@Controller
 public class ParfumController {
 
     @Autowired
     private ParfumRepository parfumRepository;
 
+    // Kezdőlap kiszolgálása
     @GetMapping("/")
     public String index() {
-        return "index";  // Az `index.html` betöltése a `src/main/resources/templates` könyvtárból
+        return "index"; // Az "index.html" sablont tölti be
     }
 
+    // Parfümök listázása
+    @GetMapping("/parfums")
+    public String getParfums(@RequestParam(required = false) List<String> genders, Model model) {
+        List<Parfum> parfums;
+
+        if (genders == null || genders.isEmpty()) {
+            parfums = parfumRepository.findAll(); // Ha nincs szűrés, akkor minden parfümöt visszaad
+        } else {
+            try {
+                List<Parfum.Gender> genderEnums = genders.stream()
+                        .map(gender -> Parfum.Gender.valueOf(gender.toUpperCase())) // Szűrjük nemek szerint
+                        .collect(Collectors.toList());
+                parfums = parfumRepository.findByGenderIn(genderEnums);
+            } catch (IllegalArgumentException e) {
+                throw new IllegalArgumentException("Invalid gender value(s) provided. Valid values are FÉRFI, NŐI, UNISEX.");
+            }
+        }
+
+        model.addAttribute("parfums", parfums); // Parfüm adatokat adunk a modellhez
+        return "parfums"; // Az "parfums.html" sablont tölti be
+    }
 }
