4450929436f9a1b9d70c5030acb75f98ae54a962
Bubenkó Zsolt <bubizsolti@mailbox.unideb.hu>
2024-12-22 01:47:53 +0100

    A parfümök már külön megjeleníthetők, ezenkívül a kinézet módosítása

--- src/main/java/parfumApp/Controller/ParfumsController.java
+++ src/main/java/parfumApp/Controller/ParfumsController.java
@@ -1,9 +1,12 @@
 package parfumApp.Controller;
 
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import parfumApp.Repository.ParfumRepository;
 import parfumApp.model.Parfum;
 import java.util.List;
@@ -28,6 +31,19 @@ public class ParfumsController {
         } catch (Exception e) {
             model.addAttribute("error", "Hiba történt a parfümök betöltése során.");
             return "error";  // Hibaoldal
+        }
+    }
+
+    @GetMapping("/parfum/{id}")
+    public String getParfumDetails(@PathVariable Long id, Model model) {
+        Optional<Parfum> parfumOptional = parfumRepository.findById(id);
+        
+        if (parfumOptional.isPresent()) {
+            model.addAttribute("parfum", parfumOptional.get());
+            return "parfumDetails";
+        } else {
+            model.addAttribute("error", "A parfüm nem található.");
+            return "error";
         }
     }
 }
